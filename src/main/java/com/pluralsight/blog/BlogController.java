3e11d4bf06dc02618d6ad441d8ce73c92d4fd71e
tvert <tvert@users.noreply.github.com>
2022-08-27 00:23:20 +0200

    Added list of Posts

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,26 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepo) {
+        postRepository = postRepo;
+    }
+
+    @RequestMapping("/")
+    public String listPosts(ModelMap modelMap) {
+        List<Post> allPosts = postRepository.getAllPosts();
+        modelMap.put("posts", allPosts);
+        return "home";
+    }
 }
