e4a973b450f22dc42737b752f88827b8db77b2b4
tvert <tvert@users.noreply.github.com>
2022-08-26 23:38:55 +0200

    Added ListPost to the Controller

--- src/main/java/com/pluralsight/blog/BlogApplication.java
+++ src/main/java/com/pluralsight/blog/BlogApplication.java
@@ -2,8 +2,6 @@ package com.pluralsight.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -12,11 +10,5 @@ public class BlogApplication {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String listPosts(ModelMap modelMap)
-	{
-		modelMap.put("title", "Blog Post 1");
-		return "Home";
-	}
 }
 
