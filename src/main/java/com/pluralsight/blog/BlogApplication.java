e34f6bf7eef676a92c5d0f13206948f60be6e5ee
tvert <tvert@users.noreply.github.com>
2022-08-26 23:23:40 +0200

    Added listPosts

--- src/main/java/com/pluralsight/blog/BlogApplication.java
+++ src/main/java/com/pluralsight/blog/BlogApplication.java
@@ -2,6 +2,8 @@ package com.pluralsight.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -10,4 +12,11 @@ public class BlogApplication {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String listPosts(ModelMap modelMap)
+	{
+		modelMap.put("title", "Blog Post 1");
+		return "Home";
+	}
 }
+
